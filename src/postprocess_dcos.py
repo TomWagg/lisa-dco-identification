c7ebe36ba3187fce61f7e398c5c37d95e5916e5d
Tom Wagg <tomjwagg@gmail.com>
2026-08-06 15:03:18 -0400

    add postprocessing script

---
+++ src/postprocess_dcos.py
@@ -0,0 +1,105 @@
+from cosmic.output import load_initC
+import h5py as h5
+import os
+import pandas as pd
+import numpy as np
+from time import time
+import argparse
+
+kstar_masks = {
+    "NSWD": ([13], [10, 11, 12]),
+    "NSNS": ([13], [13]),
+    "BHWD": ([14], [10, 11, 12]),
+    "BHNS": ([14], [13]),
+    "BHBH": ([14], [14])
+}
+FOLDER = "/mnt/ceph/users/twagg/lisa-dcos/fiducial"
+
+
+def postprocess(dco_type):
+    start_time = time()
+    print(f"Processing {dco_type}...")
+    kstars = kstar_masks[dco_type]
+
+    files = []
+    for file in os.listdir(FOLDER):
+        if file.startswith(f"{dco_type}_Z_"):
+            files.append(file)
+
+    formation_rows_list = []
+
+    files = np.sort(files)
+    for file in files:
+        file_start_time = time()
+        print(f"\n\nStarting {file}")
+
+        # load initC and bpp
+        initC = load_initC(f"{FOLDER}/{file}", key="initC")
+        bpp = pd.read_hdf(f"{FOLDER}/{file}", key="bpp")
+
+        print(f"    [{time() - file_start_time:.2f}s] Loaded initC and bpp")
+        lap = time()
+
+        # find the DCO formation rows
+        k_lo, k_hi = kstars
+        mask = ((
+            (bpp["kstar_1"].isin(k_lo)) & (bpp["kstar_2"].isin(k_hi))
+        ) | (
+            (bpp["kstar_1"].isin(k_hi)) & (bpp["kstar_2"].isin(k_lo))
+        )) & (bpp["sep"] > 0)
+
+        # if there are none then skip
+        if mask.sum() == 0:
+            print(f"  No {dco_type} systems found in this file.")
+        else:
+            print(f"    [{time() - lap:.2f}s] Found {mask.sum()} {dco_type} systems in this file.")
+            lap = time()
+
+            # otherwise record the formation rows and their metallicities
+            formation_rows = bpp[mask].drop_duplicates(subset="bin_num")[["tphys", "mass_1", "mass_2", "kstar_1", "kstar_2", "sep", "ecc", "bin_num"]]
+            formation_rows["metallicity"] = initC["metallicity"].iloc[0]
+
+            # read in the AIS weights from STROOPWAFEL
+            with h5.File(f"{FOLDER}/{file}", "r") as f:
+                formation_rows["weights"] = f["stroopwafel"]["weights"][...][f["stroopwafel"]["is_hit"][...]]
+
+            print(f"    [{time() - lap:.2f}s] Found formation rows and weights")
+            lap = time()
+
+            # find out whether each binary would have been masked in the pessimistic CE
+            dco_forming_bpp = bpp.loc[formation_rows["bin_num"]]
+
+            pessimistic = (dco_forming_bpp["evol_type"] == 7) & (
+                ((dco_forming_bpp["RRLO_1"] > 1) & (dco_forming_bpp["kstar_1"].isin([0,1,2,7,8,10,11,12]))) |
+                ((dco_forming_bpp["RRLO_1"] > 2) & (dco_forming_bpp["kstar_2"].isin([0,1,2,7,8,10,11,12])))
+            )
+            fail_pessimistic = dco_forming_bpp[pessimistic]["bin_num"]
+            formation_rows["remove_if_pessimistic"] = formation_rows["bin_num"].isin(fail_pessimistic)
+
+            formation_rows_list.append(formation_rows)
+
+        print(f"  [{time() - file_start_time:.2f}s] Finishing processing {file}")
+
+    all_formation_rows = pd.concat(formation_rows_list, ignore_index=True)
+    all_formation_rows.to_hdf(f"{FOLDER}/{dco_type}_formation_rows.h5", key="formation_rows", mode="w")
+
+    kick_infos = [pd.read_hdf(f"{FOLDER}/{file}", key="kick_info")[
+        ["tphys", "star", "delta_vsysx_1", "delta_vsysy_1", "delta_vsysz_1", "bin_num"]
+    ] for file in os.listdir(FOLDER) if file.startswith(f"{dco_type}_Z_")]
+    all_kick_infos = pd.concat(kick_infos, ignore_index=True)
+    all_kick_infos.index = all_kick_infos.index.values // 2
+    all_kick_infos.to_hdf(f"{FOLDER}/{dco_type}_kick_info.h5", key="kick_info", mode="w")
+
+    print(f"Finished processing {dco_type} in {time() - start_time:.2f}s")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Postprocess DCOS")
+    parser.add_argument("-d", "--dco_type", type=str, choices=kstar_masks.keys(), help="Type of DCO to process")
+    args = parser.parse_args()
+
+    postprocess(args.dco_type)
+
+
+if __name__ == "__main__":
+    main()
