a210cc6de028300433ceac466bb01758dec5a38e
Tom Wagg <tomjwagg@gmail.com>
2026-09-07 15:03:53 -0400

    account for RLOF in postprocessing

--- src/postprocess_dcos.py
+++ src/postprocess_dcos.py
@@ -1,4 +1,4 @@
-from cosmic.output import load_initC
+from cosmic.output import load_initC, save_initC
 import h5py as h5
 import os
 import pandas as pd
@@ -28,6 +28,7 @@ def postprocess(dco_type, folder):
             files.append(file)
 
     formation_rows_list = []
+    initC_list = []
 
     files = np.sort(files)
     for file in files:
@@ -57,7 +58,8 @@ def postprocess(dco_type, folder):
             lap = time()
 
             # otherwise record the formation rows and their metallicities
-            formation_rows = bpp[mask].drop_duplicates(subset="bin_num")[["tphys", "mass_1", "mass_2", "kstar_1", "kstar_2", "sep", "ecc", "bin_num"]]
+            formation_rows_all_cols = bpp[mask].drop_duplicates(subset="bin_num")
+            formation_rows = formation_rows_all_cols[["tphys", "mass_1", "mass_2", "kstar_1", "kstar_2", "sep", "ecc", "bin_num"]]
             formation_rows["metallicity"] = initC["metallicity"].iloc[0]
 
             # read in the AIS weights from STROOPWAFEL
@@ -77,12 +79,33 @@ def postprocess(dco_type, folder):
             fail_pessimistic = dco_forming_bpp[pessimistic]["bin_num"]
             formation_rows["remove_if_pessimistic"] = formation_rows["bin_num"].isin(fail_pessimistic)
 
+            # if the DCO contains a WD, record the time at which RLOF occurs if it does (NaN otherwise)
+            if dco_type in ["NSWD", "BHWD"]:
+                formation_rows["rlof_time"] = np.nan
+                formation_rows["rlof_sep"] = np.nan
+                rlof_stats = dco_forming_bpp[["tphys", "sep"]][((
+                    (dco_forming_bpp["kstar_1"].isin(k_lo)) & (dco_forming_bpp["kstar_2"].isin(k_hi))
+                ) | (
+                    (dco_forming_bpp["kstar_1"].isin(k_hi)) & (dco_forming_bpp["kstar_2"].isin(k_lo))
+                )) & (dco_forming_bpp["sep"] > 0) & (dco_forming_bpp["evol_type"] == 3)]
+                formation_rows.loc[rlof_stats.index, "rlof_time"] = rlof_stats["tphys"]
+                formation_rows.loc[rlof_stats.index, "rlof_sep"] = rlof_stats["sep"]
+
+                # record the initC at DCO formation for each binary
+                initC_at_formation = initC.loc[formation_rows_all_cols["bin_num"]]
+                shared_columns = initC.columns.intersection(formation_rows_all_cols.columns)
+                initC_at_formation[shared_columns] = formation_rows_all_cols[shared_columns]
+                initC_list.append(initC_at_formation)
+
             formation_rows_list.append(formation_rows)
 
         print(f"  [{time() - file_start_time:.2f}s] Finishing processing {file}")
 
     all_formation_rows = pd.concat(formation_rows_list, ignore_index=True)
     all_formation_rows.to_hdf(f"{folder}/{dco_type}_formation_rows.h5", key="formation_rows", mode="w")
+
+    all_initC = pd.concat(initC_list, ignore_index=True)
+    save_initC(f"{folder}/{dco_type}_initC.h5", all_initC, key="initC")
 
     kick_infos = [pd.read_hdf(f"{input_folder}/{file}", key="kick_info")[
         ["tphys", "star", "delta_vsysx_1", "delta_vsysy_1", "delta_vsysz_1", "bin_num"]
